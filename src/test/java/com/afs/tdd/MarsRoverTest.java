8949209ca13a5add89bfb12f88c65cd1df7d920e
Theo Lo <theo.lo@oocl.com>
2020-11-26 17:06:03 +0800

    [MarsRover][Theo]test: Create unit test for command M given 0 0 W

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -18,4 +18,18 @@ public class MarsRoverTest {
         assertEquals(1, marsRover.getLocationY());
         assertEquals("N", marsRover.getDirection());
     }
+
+    @Test
+    public void should_return_x_minus_1_when_executeCommands_given_0_0_W_and_M(){
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+
+        //when
+        marsRover.executeCommands("M");
+
+        //then
+        assertEquals(-1, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getDirection());
+    }
 }
